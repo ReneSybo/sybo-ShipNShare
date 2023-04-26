c5b4eaf7a08916c41d8902626890681f8848f973
René Jensen <rene.jensen@sybogames.com>
2023-04-26 15:51:36 +0200

    First simple try at gameplay

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace Classroom
+{
+	public class CameraController : MonoBehaviour
+	{
+		[SerializeField] Transform _target;
+		[SerializeField] float _distance;
+		
+		Transform _transform;
+		Vector3 _backwards;
+
+		void Awake()
+		{
+			_transform = transform;
+			_backwards = -_transform.forward;
+		}
+
+		void Update()
+		{
+			_transform.position = _target.position + (_backwards * _distance);
+		}
+
+		[ContextMenu("Update")]
+		void ForceUpdate()
+		{
+			Awake();
+			Update();
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using UnityEngine;
+
+namespace Classroom
+{
+	public class PlayerBehavior : MonoBehaviour
+	{
+		[SerializeField] Rigidbody _body;
+		[SerializeField] float _speed;
+		[SerializeField] float _maxVelocity = 99999;
+		[SerializeField] float _currentVelocity;
+		
+		void Update()
+		{
+			Vector3 movement = GetMovement();
+			_body.AddForce(movement);
+		
+			Vector3 velocity = _body.velocity;
+			float velocityLength = velocity.sqrMagnitude;
+			if (velocityLength > _maxVelocity)
+			{
+				_body.velocity = velocity * (_maxVelocity / velocityLength);
+			}
+
+			_currentVelocity = velocityLength;
+		}
+
+		Vector3 GetMovement()
+		{
+			Vector3 movement = Vector3.zero;
+			
+			if (Input.GetKey(KeyCode.W))
+			{
+				movement.z += _speed * Time.deltaTime;
+			}
+			else if (Input.GetKey(KeyCode.S))
+			{
+				movement.z -= _speed * Time.deltaTime;
+			}
+			
+			if (Input.GetKey(KeyCode.D))
+			{
+				movement.x += _speed * Time.deltaTime;
+			}
+			else if (Input.GetKey(KeyCode.A))
+			{
+				movement.x -= _speed * Time.deltaTime;
+			}
+
+			return movement;
+		}
+
+		void OnControllerColliderHit(ControllerColliderHit hit)
+		{
+			Rigidbody body = hit.rigidbody;
+			if (body != null)
+			{
+				Vector3 movement = GetMovement();
+				Vector3 position = body.position;
+				position.y *= 1.5f;
+				
+				Debug.DrawLine(transform.position, position);
+
+				body.AddForceAtPosition(movement * -100, position);
+			}
+		}
+	}
+}
