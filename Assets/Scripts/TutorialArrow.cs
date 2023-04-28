3bd48badedf153ab8c1447d88cdd37710a2b0a71
René Jensen <rene.jensen@sybogames.com>
2023-04-28 11:53:41 +0200

    Player cannot escape the room and tutorial arrow

---
+++ Assets/Scripts/TutorialArrow.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Classroom
+{
+	public class TutorialArrow : MonoBehaviour
+	{
+		MeshRenderer _renderer;
+
+		void Awake()
+		{
+			GameEvents.MovementUnlocked.AddListener(OnCanMove);
+			GameEvents.QuitGame.AddListener(OnQuitGame);
+
+			_renderer = GetComponent<MeshRenderer>();
+		}
+
+		void OnQuitGame()
+		{
+			_renderer.enabled = true;
+		}
+
+		void OnCanMove()
+		{
+			StartCoroutine(DelayedHide());
+		}
+
+		IEnumerator DelayedHide()
+		{
+			yield return new WaitForSecondsRealtime(1f);
+
+			_renderer.enabled = false;
+		}
+	}
+}
