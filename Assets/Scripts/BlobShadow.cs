6c2af7ad892e5b3a5163b09c18bea90deb3ad408
René Jensen <rene.jensen@sybogames.com>
2023-04-28 09:02:48 +0200

    Blob shadows and physics layers

---
+++ Assets/Scripts/BlobShadow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace Classroom
+{
+	public class BlobShadow : MonoBehaviour
+	{
+		Transform _transform;
+
+		[SerializeField] Transform _parentTransform;
+		float _originalY;
+		Quaternion _originalRotation;
+
+		void Awake()
+		{
+			_transform = transform;
+			_originalY = _transform.position.y;
+			_originalRotation = _transform.rotation;
+		}
+
+		void Update()
+		{
+			Vector3 parentPos = _parentTransform.position;
+			parentPos.y = _originalY; 
+			
+			_transform.position = parentPos;
+			_transform.rotation = _originalRotation;
+		}
+	}
+}
